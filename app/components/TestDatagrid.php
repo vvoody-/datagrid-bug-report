83c0a38d03e94cb8a786b3c6d477b4ede5bccf80
Samo Lisý <g.vvoody@gmail.com>
2017-06-20 21:56:59 +0200

    ajax + filter

--- app/components/TestDatagrid.php
+++ app/components/TestDatagrid.php
@@ -10,20 +10,32 @@ class TestDatagrid extends Component
 
 	public function createComponentGrid()
 	{
+		DataGrid::$icon_prefix = 'glyphicon glyphicon-';
 		$grid = new DataGrid();
 
 		$grid->setPrimaryKey(0);
+		$grid->setRememberState();
 		$grid->setDataSource([
 			[321, 54968, 2135, 516],
 			[154, 53874, 215, 484],
 			[1654, 546, 564, 8748],
 			[1651, 56416, 15651, 5651],
+			[321, 315365, 3513, 6511],
+			[13561, 31658, 1351, 3515],
 		]);
 
-		$grid->addColumnText(0, 0);
-		$grid->addColumnText(1, 1);
-		$grid->addColumnText(2, 2);
-		$grid->addColumnText(3, 3);
+		$grid->addColumnText(0, 0)
+			->setSortable()
+			->setFilterText();
+		$grid->addColumnText(1, 1)
+			->setSortable()
+			->setFilterText();
+		$grid->addColumnText(2, 2)
+			->setSortable()
+			->setFilterText();
+		$grid->addColumnText(3, 3)
+			->setSortable()
+			->setFilterText();
 
 		return $grid;
 	}
